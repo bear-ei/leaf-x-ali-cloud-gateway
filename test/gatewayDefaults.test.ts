0f378577af7a770dd48094a7a28ac725fe268462
sound shell <sound_shell@outlook.com>
2022-02-08 23:34:16 +0800

    docs: update the documentation

--- test/gatewayDefaults.test.ts
+++ test/gatewayDefaults.test.ts
@@ -2,7 +2,7 @@ import * as assert from 'assert';
 import {DEFAULTS as defaults, handleDefaults} from '../src/defaults';
 
 describe('test/defaults.test.ts', () => {
-  it('should set the global default parameters.', async () => {
+  it('should set the global default params.', async () => {
     handleDefaults({
       headers: {
         'content-Type': 'application/json',
